53b45314e3b87c76cf76eda7a5f344fe49bca4ba
msayracode <msayracode@gmail.com>
2026-02-09 09:57:23 +0300

    URLS and Views

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 # functions or classes activate url go to and trigger
 
+def home(request):
+    return HttpResponse('Home Page')
+
+def products(request):
+    return HttpResponse('Contact Page')
+
+def customer(request):
+    return HttpResponse('Customer Page')
