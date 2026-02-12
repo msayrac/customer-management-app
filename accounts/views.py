a17b9efafebd9335558453b70fe6251464124b5c
msayracode <msayracode@gmail.com>
2026-02-12 14:54:08 +0300

    Rendering Data to Templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 # functions or classes activate url go to and trigger
 
 def home(request):
+    orders =Order.objects.all()
+    customers = Customer.objects.all()
+    context = {'orders':orders, 'customers':customers}
     # return HttpResponse('Home Page')
-    return render(request,'accounts/dashboard.html')
+    return render(request,'accounts/dashboard.html', context)
 
 def products(request):
+    products = Product.objects.all()
+
     # return HttpResponse('Contact Page')
-    return render(request,'accounts/products.html')
+    return render(request,'accounts/products.html', {'products':products})
 
 def customer(request):
     # return HttpResponse('Customer Page')
