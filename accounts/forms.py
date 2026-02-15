bada3b40e4a2bf0e3116ca28458133b01612c448
msayracode <msayracode@gmail.com>
2026-02-15 16:56:04 +0300

    Create Update & Delete (CRUD) with Model Forms

---
+++ accounts/forms.py
@@ -0,0 +1,21 @@
+from django.forms import ModelForm
+from .models import Order
+
+class OrderForm(ModelForm):
+    class Meta:
+        model = Order
+        fields = '__all__'
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
