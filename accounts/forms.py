9fc7c73996c044e86da63e6496a14e10e6ffed91
msayracode <msayracode@gmail.com>
2026-02-18 23:52:08 +0300

    register page js css html added

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,10 @@
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from django import forms
 from .models import Order
+
 
 class OrderForm(ModelForm):
     class Meta:
@@ -7,6 +12,10 @@ class OrderForm(ModelForm):
         fields = '__all__'
         
 
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
 
 
 
