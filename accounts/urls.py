c186f82c7f7a01703bc12b25c5e8893f714158f0
msayracode <msayracode@gmail.com>
2026-02-19 14:54:51 +0300

    User Registration and Login Authentication are completed

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,9 @@ from  accounts import views
 
 urlpatterns = [
 
-    path('register/', views.register, name='register'),
-    path('login/',views.login, name='login'),
+    path('register/', views.registerPage, name='register'),
+    path('login/',views.loginPage, name='login'),
+    path('logout/',views.logoutUser, name='logout'),
 
     path('',views.home, name="home"),
     path('products/', views.products, name="products"),
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
 
 from .models import *
 from .forms import OrderForm, CreateUserForm
@@ -12,22 +16,49 @@ from .filters import OrderFilter
 # Create your views here.
 # functions or classes activate url go to and trigger
 
-def register(request):
-    form = CreateUserForm()
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:        
+        form = CreateUserForm()
 
-    if request.method == "POST":
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
+        if request.method == "POST":
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request,'Account was created for ' + user)
+                return redirect('login')
 
-    context ={'form':form}
-    return render(request, 'accounts/register.html', context)
+        context ={'form':form}
+        return render(request, 'accounts/register.html', context)
 
-def login(request):
-    context= {}
-    return render(request, 'accounts/login.html',context)
-    
 
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:   
+        if request.method == "POST":
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+
+            user = authenticate(request,username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request,'Username Or password is incorrect')
+        context= {}
+        return render(request, 'accounts/login.html',context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
 def home(request):
     orders =Order.objects.all()
     customers = Customer.objects.all()
@@ -50,7 +81,7 @@ def products(request):
     # return HttpResponse('Contact Page')
     return render(request,'accounts/products.html', {'products':products})
 
-
+@login_required(login_url='login')
 def customer(request, pk_test):
     customer = Customer.objects.get(id=pk_test)
 
@@ -65,6 +96,7 @@ def customer(request, pk_test):
     return render(request, 'accounts/customer.html',context)
 
 
+@login_required(login_url='login')
 def createOrder(request, pk):
     OrderFormSet = inlineformset_factory(Customer, Order, fields=('product','status'), extra=10)
     customer = Customer.objects.get(id=pk)
@@ -83,6 +115,7 @@ def createOrder(request, pk):
     return render(request, 'accounts/order_form.html', context)
 
 
+@login_required(login_url='login')
 def updateOrder(request, pk):
     order = Order.objects.get(id=pk)
     form = OrderForm(instance=order)
@@ -96,6 +129,7 @@ def updateOrder(request, pk):
     return render(request, 'accounts/order_form.html', context)
 
 
+@login_required(login_url='login')
 def deleteOrder(request, pk):
     order = Order.objects.get(id=pk)
 
