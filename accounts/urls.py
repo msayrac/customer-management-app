9f92e0627c1d331523b794323a210ec9a73312da
msayracode <msayracode@gmail.com>
2026-02-25 12:29:53 +0300

    Password reset email

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from  accounts import views
 
 urlpatterns = [
@@ -17,7 +18,19 @@ urlpatterns = [
     path('create_order/<str:pk>/', views.createOrder, name='create_order'),
     path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
     path('delete_order/<str:pk>/', views.deleteOrder, name ='delete_order'),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name="password_reset"),
+
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+   
+
 ]
+
+
 
 
 
