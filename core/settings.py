1e4c72d6023a09f80d46aeb8801b593f60e80195
msayracode <msayracode@gmail.com>
2026-02-26 12:51:20 +0300

    Password Reset Email

--- core/settings.py
+++ core/settings.py
@@ -133,9 +133,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
+EMAIL_HOST_USER = 'your email'
+EMAIL_HOST_PASSWORD = 'your password'
 
 
 
